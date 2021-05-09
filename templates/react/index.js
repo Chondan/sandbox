55c91fd7660cd3582c5dce40a8d40bec813a6c0c
chondan <chondansusuwan@outlook.com>
2021-05-09 14:57:33 +0700

    Update react template

--- templates/react/index.js
+++ templates/react/index.js
@@ -1,6 +1,6 @@
-import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader/root';
 
 const App = () => (
     <div>
@@ -9,6 +9,6 @@ const App = () => (
 );
 
 // eslint-disable-next-line react/no-render-return-value
-const render = (Component) => ReactDOM.render(<Component />, document.getElementById('root'));
+const render = (Component) => ReactDOM.hydrate(<Component />, document.getElementById('root'));
 
 render(hot(App));
